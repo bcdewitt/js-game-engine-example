2e24cf86aea3b4580fc892404815114694452b26
BDawgonnit <dewitt_bc@hotmail.com>
2018-04-17 23:19:29 -0400

    Further simplified the Game class

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -1,11 +1,7 @@
 import Game from './js-game-engine/esm/index.mjs'
 import createSceneFactory from './sceneFactory.mjs'
 
-const sceneFactory = createSceneFactory()
-
-;(async () => {
-	window.game = Game.createGame()
-		.setAssetFetcher(Game.createAssetFetcher())
-		.setScene('level-1', await sceneFactory.create('level-1'))
-		.run('level-1')
-})()
+window.game = Game.createGame()
+	.setSceneFactory(createSceneFactory())
+	.setAssetFetcher(Game.createAssetFetcher())
+	.run('level-1')
